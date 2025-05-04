ff56e0df78b7ccb949d703a509535e5df4970f2a
AbrarZaved <abrarzaved2002@gmail.com>
2025-05-04 18:36:45 +0600

    Update .gitignore to include .env and script.py; remove script.py file

--- script.py
+++
@@ -1,7 +0,0 @@
-import requests
-import finnhub
-
-finnhub_client = finnhub.Client(api_key="d0bfkj1r01qo0h63hsjgd0bfkj1r01qo0h63hsk0")
-data=finnhub_client.crypto_symbols("BINANCE")
-symbols = [item['symbol'] for item in data]
-print(symbols)
